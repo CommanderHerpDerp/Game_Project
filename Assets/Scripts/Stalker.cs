cf4014d264afe54619e76714fa0c8ae7b2f38bc2
AdamRidley <adamridley11@hotmail.com>
2015-05-31 22:01:07 +0100

    Fixed Stalker

--- Assets/Scripts/Stalker.cs
+++ Assets/Scripts/Stalker.cs
@@ -5,47 +5,46 @@ using System.Collections.Generic;
 public class Stalker : MonoBehaviour {
 	private NavMeshAgent agent;
 	private List<Vector3> orders;
-	private Vector3 Homeposition;
-	private float Hungertime=5;
-	private float Hungerclock;
-	private GameObject currentanimal;
+	private Vector3 homePosition;
+	private float hungerTime=5;
+	private float hungerClock;
+	private GameObject currentAnimal;
 	private int i=0;
-	private bool HadFirstUpdate;
+	private bool hadFirstUpdate;
 
 	// Use this for initialization
 	void Start () {
-		Homeposition = transform.position;
+		homePosition = transform.position;
 		agent = GetComponent<NavMeshAgent> ();
-		agent.destination = Homeposition;
+		agent.destination = homePosition;
 		orders = new List<Vector3> ();
-		orders.Add (Homeposition);
+		orders.Add (homePosition);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//hack to stop derpy moving on spawn
-		if (!HadFirstUpdate) {
-			if (Vector3.Distance (transform.position, Homeposition) > 2) {
+		if (!hadFirstUpdate) {
+			if (Vector3.Distance (transform.position, homePosition) > 2) {
 				print ("Fixed Derp");
 			}
-			HadFirstUpdate = true;
-			transform.position = Homeposition;
+			hadFirstUpdate = true;
+			transform.position = homePosition;
 		}
 		
 		//check if at next point
 		if (agent.remainingDistance < agent.stoppingDistance) {
 			//move to next point or back to first if at the last point
 
-			Hungerclock += Time.deltaTime;
-			if (Hungertime >= Hungerclock) {
+			hungerClock += Time.deltaTime;
+			if (hungerClock >= hungerTime) {
 				if (orders.Count != 0) {
 					if (i == 0) {
 						FindAnimalToKill ();
 					}
 					if (i == 1) {
-						DestroyParentGameObject HuntScript = currentanimal.GetComponent<DestroyParentGameObject> ();
-						HuntScript.DestroyObj ();
-						Hungerclock = 0;
+						DestroyParentGameObject huntScript = currentAnimal.GetComponent<DestroyParentGameObject> ();
+						huntScript.DestroyObj ();
 					}
 				
 					if (i == orders.Count - 1)
@@ -55,44 +54,43 @@ public class Stalker : MonoBehaviour {
 				
 
 					//next destination
+					hungerClock = 0;
 					agent.destination = orders [i];
 				}
 			}
 		}
 	}
 		void FindAnimalToKill(){
-		currentanimal = FindNearestAnimal(250);
-		if(currentanimal!=null){	
-			currentanimal.tag = "Animal.tagged";
-			print (currentanimal.name);
+		currentAnimal = FindNearestAnimal(250);
+		if(currentAnimal!=null){	
+			currentAnimal.tag = "Animal.tagged";
+			print (currentAnimal.name);
 			orders.Clear();
-			orders.Add(Homeposition);
-			orders.Add(currentanimal.transform.position);
+			orders.Add(homePosition);
+			orders.Add(currentAnimal.transform.position);
 		}
 		else
 		{
 			orders.Clear();
-			orders.Add (Homeposition);
+			orders.Add (homePosition);
 		}
 	}
 	GameObject FindNearestAnimal(float radius){
-		GameObject currentanimal=null;
+		GameObject currentAnimal=null;
 		float currentDist=radius+1;
 		Collider[] animalColliders = Physics.OverlapSphere (transform.position, radius);
 		foreach (Collider animalCollider in animalColliders)
 		{
 			if(animalCollider.gameObject.CompareTag("Animal")){
-				if(!(animalCollider.gameObject == gameObject)){
 					if (Vector3.Distance( animalCollider.gameObject.transform.position,transform.position)< currentDist){
-						currentanimal=animalCollider.gameObject;
+						currentAnimal=animalCollider.gameObject;
 						currentDist=Vector3.Distance( animalCollider.gameObject.transform.position,transform.position);
 						
 					}
-				}
 			}
 			
 		}
-		return currentanimal;
+		return currentAnimal;
 	}
 
 }
