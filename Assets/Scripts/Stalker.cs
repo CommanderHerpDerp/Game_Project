32911220cc76393f303e7588a1b18bdd29f56d5c
Angry-Raziel <prichards404@gmail.com>
2015-06-03 00:33:28 +0100

    Stalker updates, start of frenzy script, humanoid tags

--- Assets/Scripts/Stalker.cs
+++ Assets/Scripts/Stalker.cs
@@ -4,14 +4,29 @@ using System.Collections.Generic;
 
 public class Stalker : MonoBehaviour {
 	private NavMeshAgent agent;
-	private List<Vector3> orders;
+	public class humans
+	{
+		public List<GameObject>humanTargets;
+		public List<Vector3> humanTargetsPos;
+		public float humanTargetsDist;
+		public humans(List<GameObject> humanTs,List<Vector3> humanPs, float humanDs){
+			humanTargets=humanTs;
+			humanTargetsPos=humanPs;
+			humanTargetsDist=humanDs;
+			}
+	}
+	public List<humans> humansL;
 	public float hungerTime=15;
 	private float hungerClock;
+	private float distClock;
 	private GameObject currentAnimal;
+	private GameObject curretHuman;
 	private float animalDistance;
 	public Material changeMaterial1;
 	public Material changeMaterial2;
+	public Material changeMaterial3;
 	public Renderer rend;
+	private int i=0;
 
 	void Start () {
 		agent = GetComponent<NavMeshAgent> ();
@@ -25,9 +40,10 @@ void Update () {
 
 		if (currentAnimal == null)
 			FindAnimalToKill ();
-	//this is too much variable setting being done inside void update. only do this on a timer like it is from line 40 onwards so it doesnt run every update.
-	//distance calculations are computationaly expensive and should be run more sparingly, once a second would be ok! You can create another timer like distanceCheckTimer.
-		if (currentAnimal) {
+	
+		float distRefresh = 5;
+		distClock += Time.deltaTime;
+		if (currentAnimal&& (distClock>=distRefresh)){
 			animalDistance = Vector3.Distance (agent.transform.position, currentAnimal.transform.position);
 
 			if (animalDistance > 15) {
@@ -37,7 +53,7 @@ void Update () {
 		
 
 		}
-		if (hungerClock >= hungerTime) {
+		if (hungerClock >= hungerTime && currentAnimal) {
 			agent.destination = currentAnimal.transform.position;
 
 			if (animalDistance <= 15) {
@@ -51,13 +67,21 @@ void Update () {
 				hungerClock = 0;
 			}
 		}
+		if (hungerClock > (hungerTime*10)) {
+			Rampage();
+			//float humanDistance = Vector3.Distance (agent.transform.position,humansL);
+
+		//	if (humanDistance < 2){
+				//DestroyParentGameObject humanScript = humanTarget.GetComponent<DestroyParentGameObject> ();
+		//		humanScript.DestroyObj ();
+		//		hungerClock = 0;
+		//	}
+		}
 	}
 
 	void FindAnimalToKill(){
 		currentAnimal = FindNearestAnimal(250);
 		if(currentAnimal!=null){	
-			//orders.Clear();
-			//agent.destination = currentAnimal.transform.position;
 			print (currentAnimal.name);
 			}
 		}
@@ -66,8 +90,7 @@ void Update () {
 		GameObject currentAnimal=null;
 		float currentDist=radius+1;
 		Collider[] animalColliders = Physics.OverlapSphere (transform.position, radius);
-		foreach (Collider animalCollider in animalColliders)
-		{
+		foreach (Collider animalCollider in animalColliders){
 			if(animalCollider.gameObject.CompareTag("Animal")){
 					if (Vector3.Distance( animalCollider.gameObject.transform.position,transform.position)< currentDist){
 						currentAnimal=animalCollider.gameObject;
@@ -79,4 +102,40 @@ void Update () {
 		return currentAnimal;
 	}
 
+	void Rampage (){
+		if (humansL.Count != 0) {
+			this.rend.material=changeMaterial3;
+			agent.speed=7f;
+			if (i == 0) {
+
+			}
+
+			if (i == humansL.Count - 1)
+				i = 0;
+			else
+				i++;
+		//	agent.destination = humanTargetsPos [i];
+		}
+	}
+
+	List<Vector3> FindHumans(){
+		GameObject currentHuman;
+		Vector3 currentHumanPos;
+		float currentHumanDist;
+		humansL = new List<humans> ();
+		float currentDist = 500;
+		Collider[] humanColliders = Physics.OverlapSphere (transform.position, 500);
+		foreach (Collider humanCollider in humanColliders) {
+			if (humanCollider.gameObject.CompareTag ("Humanoid")) {
+				if (Vector3.Distance (humanCollider.gameObject.transform.position, transform.position) < currentDist) {
+					currentHuman = humanCollider.gameObject;
+					currentHumanPos = humanCollider.gameObject.transform.position;
+					currentHumanDist = Vector3.Distance (currentHuman.transform.position, transform.position);
+					humansL.Add (new humans(currentHuman,currentHumanPos,currentHumanDist));
+				}
+			}
+
+		}
+		return humansL;
+		}
 }
