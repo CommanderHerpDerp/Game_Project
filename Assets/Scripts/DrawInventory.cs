ca7991e98eda8fb23f3fef2a16b0ec30851d5ae0
AdamRidley <adamridley11@hotmail.com>
2015-06-10 15:16:40 +0100

    Changes

---
+++ Assets/Scripts/DrawInventory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawInventory : MonoBehaviour {
+	public List<GameObject> icons;
+
+	public void drawList(List<ItemManager.ItemStack> list){
+		for (int a=0; a<list.Count-1; a++) {
+			if (a<icons.Count-1){
+
+			}
+			else{
+			}
+		}
+	}
+
+	void Start(){
+		icons = new List<GameObject>();
+	}
+
+
+}
